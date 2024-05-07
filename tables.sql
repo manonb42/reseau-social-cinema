22195b462245e56b5674b113ee5498054e3ca50d
Jana Ayadi <jana@macbook-air-de-jana.home>
2024-05-07 20:45:43 +0200

    modifications

--- tables.sql
+++ tables.sql
@@ -1,123 +1,179 @@
 CREATE TABLE Films
 (
-    titre VARCHAR(100) PRIMARY KEY,
-    realisateur_id INT,
-    date_sortie DATE,
-    genre_id INT NOT NULL,
-    sous_genre_id INT,
-    FOREIGN KEY (genre_id) REFERENCES Genres(genre_id), 
-    FOREIGN KEY (sous_genre_id) REFERENCES Sous_genres(sous_genre_id),
-    FOREIGN KEY (realisateur_id) REFERENCES Artistes(artiste_id), 
-    UNIQUE(titre, realisateur_id)
+	titre VARCHAR(100),
+	realisateur_id INT,
+	date_sorie DATE,
+	PRIMARY KEY (titre)
+    	FOREIGN KEY (realisateur_id) REFERENCES Artistes(artiste_id)
+    	--UNIQUE(titre, realisateur_id) ne sert à rien car titre étant unique, (titre, realisateur_id) le sera aussi
 )
 
 CREATE TABLE Genres
 (
-    genre_id INT PRIMARY KEY,
-    label VARCHAR(100)
+	label VARCHAR(100) PRIMARY KEY
 )
 
-CREATE TABLE Sous_genres
+CREATE TABLE SousGenres
 (
-    sous_genre_id INT PRIMARY KEY,
-    genre_id INT,
-    label VARCHAR(100), 
-    FOREIGN KEY(genre_id) REFERENCES Genres(genre_id)
+	label VARCHAR(100),
+   	genre VARCHAR(100),
+	PRIMARY KEY (label),
+	FOREIGN KEY(genre) REFERENCES Genres(label)
+)
+
+CREATE TABLE AppartenancesGenres
+(
+	titre VARCHAR(100),
+	genre VARCHAR(100),
+	PRIMARY KEY (titre,genre),
+	FOREIGN KEY (titre) REFERENCES Film(titre),
+	FOREIGN KEY (genre) REFERENCES Genres(label)
+)
+
+CREATE TABLE AppartenanceSousGenre
+(
+	titre VARCHAR(100),
+	sousgenre VARCHAR(100), 
+	PRIMARY KEY (titre,sousgenre), 
+	FOREIGN KEY (titre) REFERENCES Film(titre),
+	FOREIGN KEY (sousgenre) REFERENCES SousGenres(label)
+
 )
 
 CREATE TABLE Artistes
 (
-    artiste_id INT PRIMARY KEY,
-    prenom VARCHAR(100) NOT NULL,
-    nom VARCHAR(100) NOT NULL,
-    date_naissance DATE, 
-    categorie_id INT /* est ce qu'on garde int ? */
+    	artiste_id INT PRIMARY KEY,
+   	prenom VARCHAR(100) NOT NULL,
+   	nom VARCHAR(100) NOT NULL,
+    	date_naissance DATE,
+	PRIMARY KEY (artiste_id)
 )
 
 CREATE TABLE Utilisateurs
 (
-    u_id SERIAL PRIMARY KEY,
-    categorie_id INT, /* random, artiste .. */
-    u_login VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    nom VARCHAR(100) NOT NULL, 
-    prenom VARCHAR(100) NOT NULL, 
-    mdp VARCHAR(100) NOT NULL, 
-    pays VARCHAR(100), 
-    date_naissance DATE
-    /* ajouter role_id */
+    	u_id SERIAL PRIMARY KEY,
+    	categorie VARCHAR(100), /* random, artiste .. */
+    	u_login VARCHAR(100) NOT NULL UNIQUE,
+    	email VARCHAR(100) NOT NULL UNIQUE,
+    	nom VARCHAR(100) NOT NULL, 
+    	prenom VARCHAR(100) NOT NULL, 
+    	mdp VARCHAR(100) NOT NULL, 
+    	pays VARCHAR(100), 
+    	date_naissance DATE
+)
+
+CREATE TABLE Vip
+(
+	u_id INT,
+	PRIMARY KEY (u_id)
+	FOREIGN KEY (u_id) REFERENCES Utilisateurs(u_id)
 )
 
 CREATE TABLE Follow
 (
-    u_id_followers INT NOT NULL, 
-    u_id_following INT NOT NULL
+    	u_id_follower INT, 
+    	u_id_following INT,
+	PRIMARY KEY(u_id_follower, u_id_following),
+	FOREIGN_KEY (u_id_follower) REFERENCES Utilisateurs(u_id),
+	FOREIGN KEY (u_id_following) REFERENCES Utilisateurs(u_id),
+	CHECK u_id_following <> u_id_follower
+	
+)
+
+CREATE TABLE Avis
+(
+	titre VARCHAR(100),
+	auteur_id INT,
+	commentaire text,
+	notation INT,
+	CHECK ( commentaire IS NOT NULL OR notation IS NOT NULL)
 )
 
 CREATE TABLE Publications
 (
-    publication_id INT PRIMARY KEY, 
-    date_publication DATETIME NOT NULL, 
-    auteur_id INT NOT NULL, 
-    discussion_id INT NOT NULL,
-    answer_to_publication_id INT, 
-    FOREIGN KEY (auteur_id) REFERENCES Utilisateurs(u_id), 
-    FOREIGN KEY (discussion_id) REFERENCES Discussions(discussion_id), 
-    FOREIGN KEY (answer_to_publication_id) REFERENCES Publications(publication_id)
+    	publication_id SERIAL, 
+    	date_publication DATETIME NOT NULL, 
+    	auteur_id INT NOT NULL, 
+    	discussion_id INT NOT NULL,
+	PRIMARY KEY (publication_id),
+    	FOREIGN KEY (auteur_id) REFERENCES Utilisateurs(u_id), 
+    	FOREIGN KEY (discussion_id) REFERENCES Discussions(discussion_id), 
+    	FOREIGN KEY (answer_to_publication_id) REFERENCES Publications(publication_id)
     
+)
+
+CREATE TABLE Reponses
+(
+	message_pere INT,
+	message_fil INT,
+	PRIMARY KEY (message_pere, message_fils)
+	FOREIGN KEY (message_pere) REFERENCES Publications(publication_id),
+	FOREIGN KEY (message_fils) REFERENCES Publications(publication_id),
+	CHECK message_fils > message_pere
 )
 
 CREATE TABLE Discussions
 (
-    discussion_id INT PRIMARY KEY, 
-    label VARCHAR(255) 
+    	discussion_id INT PRIMARY KEY, 
+    	label VARCHAR(255) 
 )
 
-CREATE TABLE REACTION
+CREATE TABLE Reactions
 (
-    publication_id INT NOT NULL,
-    u_id INT NOT NULL,
-    r_type VARCHAR(255) NOT NULL,
-    PRIMARY KEY(publication_id, u_id, r_type), 
-    FOREIGN KEY (u_id) REFERENCES Utilisateurs(u_id), 
-    FOREIGN KEY (publication_id) REFERENCES Publications(publication_id), 
+	publication_id INT NOT NULL,
+   	u_id INT NOT NULL,
+    	r_type VARCHAR(255) NOT NULL,
+    	PRIMARY KEY(publication_id, u_id, r_type), 
+    	FOREIGN KEY (u_id) REFERENCES Utilisateurs(u_id), 
+    	FOREIGN KEY (publication_id) REFERENCES Publications(publication_id)
 )
 
 CREATE TABLE Participation
 (
-    user_id INT, 
-    event_id INT, 
-    inscrit INT, 
-    interesse INT, /* CHECK IF INSCRIT THEN !INTERESSE ELSE INTERESSE ? */
-    FOREIGN KEY (user_id) REFERENCES Utilisateurs(u_id), 
-    FOREIGN KEY (event_id) REFERENCES Evenements_Futurs(event_id)
+	user_id INT, 
+    	event_id INT, 
+    	inscrit BOOLEAN NOT NULL, 
+    	interesse BOOLEAN NOT NULL,
+    	FOREIGN KEY (user_id) REFERENCES Utilisateurs(u_id), 
+	CHECK inscrit<>interesse
+
 )
 
 
 CREATE TABLE Archives_web
 (
-    event_id INT NOT NULL, 
-    lien_web VARCHAR(255), 
-    FOREIGN KEY (event_id) REFERENCES Evenements_Passes(event_id)
+    	event_id INT NOT NULL, 
+    	lien_web VARCHAR(255), 
+    	FOREIGN KEY (event_id) REFERENCES Evenements_Passes(event_id)
 )
 
 
 CREATE TABLE Evenements_Futurs 
 (
-    event_id INT PRIMARY KEY, 
-    nom VARCHAR(100) NOT NULL, 
-    date DATETIME CHECK (DATEDIFF(CAST(NOW() AS DATETIME), date)<=0), /* verifier que ça marche*/
-    lieu VARCHAR(255),
-    nb_places INT,
-    prix INT
+    	event_id INT PRIMARY KEY, 
+    	nom VARCHAR(100) NOT NULL, 
+    	date_event DATETIME CHECK (DATEDIFF(CAST(NOW() AS DATETIME), date)<=0), /* verifier que ça marche*/
+    	lieu VARCHAR(255),
+    	nb_places INT,
+    	prix FLOAT
 )
 
 CREATE TABLE Evenements_Passes
 (
-    event_id INT PRIMARY KEY, 
-    nom VARCHAR(100) NOT NULL, 
-    date DATETIME CHECK (DATEDIFF(CAST(NOW() AS DATETIME), date)>0), /* verifier que ça marche*/
-    lieu VARCHAR(255),
-    nb_participants INT
+    	event_id INT PRIMARY KEY, 
+    	nom VARCHAR(100) NOT NULL, 
+    	date_event DATETIME CHECK (DATEDIFF(CAST(NOW() AS DATETIME), date)>0), /* verifier que ça marche*/
+    	lieu VARCHAR(255),
+    	nb_participants INT
 )
+
+CREATE TABLE Annonce
+(
+	event_id INT UNIQUE,
+	discussion_id INT UNIQUE,
+	FOREIGN KEY (event_id) REFERENCES Evenements_Futurs(event_id),
+	FOREIGN KEY (discussion_id) REFERENCES Discussions(discussion_id),
+	
+)
+
 
