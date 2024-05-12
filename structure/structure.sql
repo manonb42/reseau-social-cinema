7fb687568ab342b5c852cca36c7ca95734dec953
Manon Baha <baha.manon@gmail.com>
2024-05-12 17:55:03 +0200

    bug fix

--- structure/structure.sql
+++ structure/structure.sql
@@ -11,22 +11,20 @@ create table Utilisateurs
 	label varchar(100),
 	primary key(u_id),
 	foreign key(label) references Categories(label) on update cascade
-	
-)
+);
 
 -- entités faibles de Utilisateurs
 create table Admins(
 	u_id int
 	primary key(u_id),
 	foreign key(u_id) references Utilisateurs(u_id) on delete cascade
-
-)
+);
 
 create table Moderateurs(
 	u_id int
 	primary key(u_id),
 	foreign key(u_id) references Utilisateurs(u_id) on delete cascade
-)
+);
 
 ----------
 
@@ -34,7 +32,7 @@ create table Categories
 (
 	label varchar(100),
 	primary key(label)
-)
+);
 
 create table Artistes
 (
@@ -44,7 +42,7 @@ create table Artistes
 	pays varchar(100),
 	birthdate date,
 	primary key(a_id)
-)
+);
 
 create table Films
 (
@@ -55,21 +53,21 @@ create table Films
 	primary key(f_id),
 	foreign key(realisateur) references Artistes(a_id)
 	--il est légalement possible qu'un même réalisateur crée plusieurs films avec exactement le même nom, c'est déjà arrivé
-)
+);
 
 create table Genres
 (
 	g_id serial,
 	label varchar(100) unique not null,
 	primary key(g_id)
-)
+);
 
 create table Discussions
 (
 	d_id serial,
 	label varchar(100) not null,
 	primary key(d_id) --plusiers discussions peuvent avoir le même nom, comme sur beaucoup de réseaux sociaux
-)
+);
 
 
 create table Publications
@@ -81,21 +79,19 @@ create table Publications
 	primary key(p_id),
 	foreign key(u_id) references Utilisateurs(u_id) on delete set null,
 	foreign key(d_id) references Discussions(d_id) on delete cascade
-)
+);
 
---entité faible de Publications
 
-create type smiley as enum ('happy', 'sad', 'angry', 'shocked', 'disgusted', 'thumb', 'heart', 'lol')
+create type smiley as enum ('happy', 'sad', 'angry', 'shocked', 'disgusted', 'thumb', 'heart', 'lol');
 
 create table Reactions
 (
 	u_id int,
 	p_id int,
 	emoji smiley not null,
-	primary key(u_id,p_id,emoji)
-	foreign key(u_id) references Utilisateurs(u_id); on delete set null,
-	
-)
+	primary key(u_id,p_id,emoji),
+	foreign key(u_id) references Utilisateurs(u_id) on delete set null,	
+);
 
 ----------
 
@@ -109,8 +105,7 @@ create table Evenements
 	primary key(e_id),
 	unique (date_event, adresse)
 	--pas deux événements différents au même endroit le même jour
-
-)
+);
 
 -- entité faible de Evenements
 
@@ -121,7 +116,7 @@ create table Archives_web
 	primary key(e_id, lien_web),
 	foreign key(e_id) references Evenements(e_id) on delete cascade
 	-- trigger evenement passé
-)
+);
 
 ----------
 
@@ -136,8 +131,7 @@ create table AvisFilms -- Films et Utilisateurs
 	foreign key(f_id) references Films(f_id) on delete cascade,
 	foreign key(u_id) references Utilisateurs(u_id) on delete set null,
 	CHECK (mark >=0 AND mark<=5)
-
-)
+);
 
 create table FilmsGenres -- Films et Genres
 (
@@ -146,39 +140,39 @@ create table FilmsGenres -- Films et Genres
 	primary key(f_id,g_id),
 	foreign key(f_id) references Films(f_id) on delete cascade,
 	foreign key(g_id) references Films(g_id) on delete cascade
-)
+);
 
 
 create table Castings --Films et Artistes
 (
 	f_id int,
 	a_id int, 
-	primary_key(f_id, u_id),
+	primary key (f_id, u_id),
 	foreign key(f_id) references Films(f_id) on delete cascade,
 	foreign key(a_id) references Artistes(a_id) on delete cascade
-)
+);
 
 create table ParentGenre -- Genres et Genres
 (
 	genre_parent int,
 	genre_enfant int,
-	primary key(genre_parent, genre_enfant)
+	primary key(genre_parent, genre_enfant),
 	foreign key(genre_parent) references Genres(g_id) on delete cascade,
 	foreign key(genre_enfant) references Genres(g_id) on delete cascade,
-	CHECK genre_parent <> genre_enfant -- le souci c'est qu'il peut y avoir le couple (A,B) et le couple (B,A)
+	CHECK (genre_parent <> genre_enfant) -- le souci c'est qu'il peut y avoir le couple (A,B) et le couple (B,A)
 	--CHECK genre_parent > genre_enfant : le souci c'est que ça demande une insertion chronologique/hiérarchique des genres
-)
+);
 
 
 create table Programme -- Films et Evenements
 (
 	e_id int,
 	f_id int,
-	primary key(e_if, f_id)
+	primary key(e_if, f_id),
 	foreign key(e_id) references Evenements(e_id) on delete cascade,
 	foreign key(f_id) references Films(f_id)
 	-- trigger evenement futur
-)
+);
 
 create table AvisEvenements --  Evenements et Utilisateurs
 (
@@ -190,7 +184,7 @@ create table AvisEvenements --  Evenements et Utilisateurs
 	foreign key(u_id) references Utilisateurs(u_id) on delete set null,
 	CHECK (mark >=0 AND mark<=5)
 	-- trigger evenement passé
-)
+);
 
 create table Participation -- Utilisateurs et Evenements
 (
@@ -201,9 +195,9 @@ create table Participation -- Utilisateurs et Evenements
 	primary key(e_id,u_id),
 	foreign key(e_id) references Evenements(e_id) on delete cascade,
 	foreign key(u_id) references Utilisateurs(u_id) on delete cascade,
-	CHECK interesse <> inscrit
+	CHECK (interesse <> inscrit)
 	-- trigger evenement futur
-)
+);
 
 create table Organisateurs -- Utilisateurs et Evenements
 (
@@ -212,7 +206,7 @@ create table Organisateurs -- Utilisateurs et Evenements
 	primary key(e_id,organisateur),
 	foreign key(e_id) references Evenements(e_id) on delete cascade,
 	foreign key(organisateur) references Utilisateurs(u_id) on delete cascade
-)
+);
 
 create table Annonces -- Discussions et Evenements
 (
@@ -220,19 +214,19 @@ create table Annonces -- Discussions et Evenements
 	d_id int unique, 
 	foreign key(e_id) references Evenements(e_id) on delete cascade,
 	foreign key(d_id) references Discussions(d_id) on delete cascade
-)
+);
 
 
 create table ParentGenre -- Genres et Genres
 (
 	genre_parent int,
 	genre_enfant int,
-	primary key(genre_parent, genre_enfant)
+	primary key(genre_parent, genre_enfant),
 	foreign key(genre_parent) references Genres(g_id) on delete cascade,
-	foreign key(genre_enfant) references Genres(g_id) on delete cascade
-	CHECK genre_parent <> genre_enfant -- le souci c'est qu'il peut y avoir le couple (A,B) et le couple (B,A)
+	foreign key(genre_enfant) references Genres(g_id) on delete cascade,
+	CHECK (genre_parent <> genre_enfant) -- le souci c'est qu'il peut y avoir le couple (A,B) et le couple (B,A)
 	--CHECK genre_parent > genre_enfant : le souci c'est que ça demande une insertion chronologique/hiérarchique des genres
-)
+);
 
 create table Follow -- Utilisateurs et Utilisateurs
 (
@@ -241,8 +235,8 @@ create table Follow -- Utilisateurs et Utilisateurs
 	primary key(follower, followed),
 	foreign key(follower) references Utilisateurs(u_id) on delete cascade,
 	foreign key(followed) references Utilisateurs(u_id) on delete cascade,
-	CHECK follower <> followed -- si (A follow B) et (B Follow A) alors on peut dire que A et B sont amis
-)
+	CHECK (follower <> followed) -- si (A follow B) et (B Follow A) alors on peut dire que A et B sont amis
+);
 
 create table Dialogue -- Publications et Publications
 (
@@ -252,10 +246,10 @@ create table Dialogue -- Publications et Publications
 	-- (1,2) (1,3) (2,4) oui car 2 et 3 sont des réponses à 1, et 4 est une réponse à 2 
 	-- (1,2) (1,3) (2,3) non car 3 est une reponse à 2 ET à 1
 	-- primary key(source, reponse) ne sert à rien car etant donne reponse unique, le couple sera unique
-	foreign_key(source) references Publications(p_id) on delete set null,
-	foreign key(reponse) references Publications(p_id) on delete cascde,
-	CHECK source < reponse
-)
+	foreign key(source) references Publications(p_id) on delete set null,
+	foreign key(reponse) references Publications(p_id) on delete cascade,
+	CHECK (source < reponse)
+);
 	
 	
 
