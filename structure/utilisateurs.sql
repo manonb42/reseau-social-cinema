fb729da262d0731c23d0d9681e428f8b148d5872
Manon Baha <baha.manon@gmail.com>
2024-05-12 19:26:52 +0200

    utilisateurs update

--- structure/utilisateurs.sql
+++ structure/utilisateurs.sql
@@ -11,8 +11,13 @@ create table Utilisateurs
 	label varchar(100),
 	primary key(u_id),
 	foreign key(label) references Categories(label) on update cascade
-	
-)
+);
+
+create table Categories
+(
+	label varchar(100),
+	primary key(label)
+);
 
 create table Follow -- Utilisateurs et Utilisateurs
 (
@@ -22,18 +27,17 @@ create table Follow -- Utilisateurs et Utilisateurs
 	foreign key(follower) references Utilisateurs(u_id) on delete cascade,
 	foreign key(followed) references Utilisateurs(u_id) on delete cascade,
 	CHECK (follower <> followed) -- si (A follow B) et (B Follow A) alors on peut dire que A et B sont amis
-)
+);
 
 -- entités faibles de Utilisateurs
 create table Admins(
 	u_id int
 	primary key(u_id),
 	foreign key(u_id) references Utilisateurs(u_id) on delete cascade
-
-)
+);
 
 create table Moderateurs(
 	u_id int
 	primary key(u_id),
 	foreign key(u_id) references Utilisateurs(u_id) on delete cascade
-)
+);
