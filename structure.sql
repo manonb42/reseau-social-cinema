1eb2fa0f07b1c9dce7c641e59a8da8a1389ad67f
Ayadi Jana <jana@macbook-air-de-jana.home>
2024-05-25 21:11:07 +0200

    re

--- structure.sql
+++ structure.sql
@@ -221,18 +221,18 @@ create table Annonces -- Discussions et Evenements
 );
 
 
-create table ParentGenre -- Genres et Genres
+create table GenresParents -- Genres et Genres
 (
-	genre_parent int,
-	genre_enfant int,
-	primary key(genre_parent, genre_enfant),
-	foreign key(genre_parent) references Genres(g_id) on delete cascade,
-	foreign key(genre_enfant) references Genres(g_id) on delete cascade,
-	CHECK (genre_parent <> genre_enfant) -- le souci c'est qu'il peut y avoir le couple (A,B) et le couple (B,A)
+	genre int,
+	sousgenre int,
+	primary key(genre, sousgenre),
+	foreign key(genre) references Genres(g_id) on delete cascade,
+	foreign key(sous) references Genres(g_id) on delete cascade,
+	CHECK (genre <> sousgenre) -- le souci c'est qu'il peut y avoir le couple (A,B) et le couple (B,A)
 	--CHECK genre_parent > genre_enfant : le souci c'est que ça demande une insertion chronologique/hiérarchique des genres
 );
 
-create table Follow -- Utilisateurs et Utilisateurs
+create table Relation -- Utilisateurs et Utilisateurs
 (
 	follower int,
 	followed int, 
